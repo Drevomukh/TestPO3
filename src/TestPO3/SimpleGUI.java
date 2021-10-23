abf3d050edf88a49e243924a444710e9f27dc585
Daniil <markevichmarkevich.ru@gmail.com>
2021-10-23 13:27:26 +0300

    Второй коммит. Добавлена панель.

--- src/TestPO3/SimpleGUI.java
+++ src/TestPO3/SimpleGUI.java
@@ -1,4 +1,18 @@
 package TestPO3;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class SimpleGUI {
+
+    JPanel panel = new JPanel();
+    JButton button1 = new JButton("AC Milan");
+    JButton button2 = new JButton("Real Madrid");
+    int i = 0;
+    int t = 0;
+    JLabel label1 = new JLabel("Result: " + i +" "+ "X" +" "+ t);
+    JLabel label2 = new JLabel("Last Scorer: ");
+    JLabel label3 = new JLabel("Winner: ");
+
+
 }
